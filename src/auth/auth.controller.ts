f3f7943a4551298281c3f825248f6d63c8ee62b9
FME849 <1414439@gmail.com>
2024-04-14 14:42:29 +0700

    feat: impl RolesGuard in addUser controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { ApiTags, ApiBody, ApiHeaders } from '@nestjs/swagger';
 import { SignInDto, SignUpDto } from './auth.dto';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { Role, Roles } from 'src/decorator/role.decorator';
+import { RolesGuard } from 'src/guard/roles.guard';
 
 @ApiTags('UserManagement')
 @Controller('auth')
@@ -24,9 +25,10 @@ export class AuthController {
   }
 
   @ApiHeaders([{ name: 'token', required: true }])
+  @Post('add-user')
   @Roles(Role.Admin)
   @UseGuards(AuthGuard)
-  getUser(@Req() req: any) {
+  @UseGuards(RolesGuard)
     console.log(req.jwtPayload);
     return 'get user';
   }
---
+++ src/guard/roles.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ROLES_KEY, Role } from 'src/decorator/role.decorator';
+import { jwtSecret } from 'src/utils/jwt';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwt: JwtService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    console.log(requiredRoles, '<<< required role');
+
+    if (requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { token } = context.switchToHttp().getRequest().headers;
+    const { userType } = this.jwt.verify(token, {
+      secret: jwtSecret,
+    });
+
+    return requiredRoles.some((role) => userType.toUpperCase === role);
+  }
+}
